a5f09d867f0d22a5c72a49f63d25a7e347c36ae2
splashjanski <splashjanski@gmail.com>
2024-02-17 08:30:18 +0400

    homework

---
+++ day 04/homework4.py
@@ -0,0 +1,5 @@
+age = input("Enter your age: ")
+username = input("Enter your username: ")
+age = int(age)  # Converting the input string to an integer
+print("Your age is:", age)
+print("your username is:", username)
