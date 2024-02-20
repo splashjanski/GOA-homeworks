b3c509844402c95c4a2adb7442cfebf6bb33979e
splashjanski <splashjanski@gmail.com>
2024-02-21 00:44:37 +0400

    homework

--- day 00/homework1.py
+++ day 00/homework1.py
@@ -2,3 +2,4 @@ name = "teodore"
 last_name = "janiashvili"
 age = 15
 print(name + " " + last_name + " " + str(age))
+ 
--- day 01/homework01.py
+++ day 01/homework01.py
@@ -17,7 +17,7 @@ left(90)
 
 forward(200)
 left(90)
-end_fill()
+end_fill() 
 
 #end of squar.
 begin_fill()
