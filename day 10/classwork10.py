e8cfbcd24a31f462636f8282d5685a9c4e14207c
splashjanski <splashjanski@gmail.com>
2024-03-14 14:08:31 +0400

    last commit

---
+++ day 10/classwork10.py
@@ -0,0 +1,24 @@
+# age = int(input("enter your age:"))
+
+# if age >= 0  and age < 13:
+#     print("you are child")
+# elif  age >= 13  and  age < 18:
+#     print("you are თინეიჯერი") 
+# elif age >= 60  and age < 60:
+#     print("18 +")
+# else:
+#     print("older")           
+ 
+
+# film = ["მწვანე ბილიკი" , "მწვანე ქუჩის ხულიგნები" , "სისხლი და ძვალი"]
+
+# print(film[1])
+
+
+
+age = 1
+
+number = 2
+
+print(age , number)
+
---
+++ day 10/homework10.py
@@ -0,0 +1,72 @@
+# programming_league = ["html", "css", "js", "python", "c++"]
+
+# for i in programming_league:
+#     print(i)
+
+# print("last programming_league:", programming_league[4])
+
+
+
+
+
+
+
+
+
+#  შექმენით სია, სადაც გექნებათ string, integer, float, boolean
+# mixed_list = ["hello", 42, 3.14, True]
+
+#  ცალ-ცალკე გამოიტანეთ ყველა ელემენტი
+# print("print all list:")
+# for i in mixed_list:
+#     print(i)
+
+
+
+
+
+
+
+# Create a list of multiples of 4 from 0 to 20
+# multiples_of_4 = [num for num in range(0, 21, 4)]
+
+# Print the list
+# print("Multiples of 4 from 0 to 20:", multiples_of_4)
+
+# Find and print the largest number
+# largest_number = max(multiples_of_4)
+# print("The largest number is:", largest_number)
+
+
+
+
+
+
+
+
+# # Create a list of odd numbers from 30 to 50
+# odd_numbers = [num for num in range(31, 51, 2)]
+
+# # Print the list
+# print("Odd numbers from 30 to 50:", odd_numbers)
+
+# # Sort the list
+# odd_numbers.sort()
+
+# # Calculate the sum of the three smallest elements
+# sum_of_smallest_three = sum(odd_numbers[:3])
+
+# # Print the sum
+# print("Sum of the three smallest elements:", sum_of_smallest_three)
+
+
+
+
+
+
+
+
+# # Print multiples of three
+# print("Multiples of three:")
+# for num in range(3, 51, 3):  # Starting from 3, up to 50, stepping by 3
+#     print(num)
