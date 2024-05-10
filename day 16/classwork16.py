93c94f9abf44ed7f2213937d28ea2a48cba5be63
splashjanski <splashjanski@gmail.com>
2024-05-10 15:53:10 +0400

    last commit

---
+++ day 16/classwork16.py
@@ -0,0 +1,8 @@
+def multiplay_by_tree(user_number):
+    print(user_number * 3)
+
+multiplay_by_tree(4)
+multiplay_by_tree(8)
+multiplay_by_tree(15)
+multiplay_by_tree(26)
+multiplay_by_tree(47)
---
+++ day 23/homework23.py
@@ -0,0 +1,7 @@
+def remove_duplicates(numbers):
+    return list(set(numbers))
+
+# Example usage:
+numbers = [1, 2, 3, 4, 3, 2, 5, 6, 5]
+filtered_numbers = remove_duplicates(numbers)
+print("Filtered numbers without duplicates:", filtered_numbers)
