59887f501c9fc8e959d2200d64c98cb37490eee6
splashjanski <splashjanski@gmail.com>
2024-08-17 13:35:29 +0400

    last commit

---
+++ GRUP-34/GOA-homeworks/day 14/classwork14.py
@@ -0,0 +1,23 @@
+
+
+
+# i = 0
+# while i < 10:
+#     print("TEDO")
+#     i += 1
+
+
+
+
+# def guess_game():
+#     while True:
+#         user_guess = int(input("Guess a number: "))
+#         if user_guess == 5:
+#             print("You won!")
+#             break
+#         else:
+#             print("You lost, try again.")
+
+# guess_game()
+
+
---
+++ GRUP-34/GOA-homeworks/day 18/classwork18.py
@@ -0,0 +1,91 @@
+
+# numbers_list = []
+
+
+# for number in range(1, 11):
+#     numbers_list.append(number)
+
+
+# list_length = len(numbers_list)
+
+# print("Numbers list:", numbers_list)
+# print("Number of elements in the list:", list_length)
+
+
+
+
+# start_number = int(input("Enter the first number: "))
+# end_number = int(input("Enter the second number: "))
+
+# numbers_list = []
+
+# for number in range(start_number + 1, end_number):
+#     numbers_list.append(number)
+
+# print("Numbers between", start_number, "and", end_number, "are:", numbers_list)
+
+
+
+
+
+# start_number = int(input("Enter the start number: "))
+# end_number = int(input("Enter the end number: "))
+
+# numbers_list = []
+
+# for number in range(start_number + 1, end_number):
+#     numbers_list.append(number)
+
+# print("Numbers between", start_number, "and", end_number, "are:", numbers_list)
+
+# if numbers_list:
+#     max_number = max(numbers_list)
+#     min_number = min(numbers_list)
+#     sum_numbers = sum(numbers_list)
+    
+#     print("Maximum number:", max_number)
+#     print("Minimum number:", min_number)
+#     print("Sum of all numbers:", sum_numbers)
+# else:
+#     print("No numbers found between", start_number, "and", end_number)
+
+
+
+# num_count = int(input("How many numbers do you want to enter? "))
+
+# numbers = []
+
+# for i in range(num_count):
+#     num = float(input(f"Enter number {i+1}: "))
+#     numbers.append(num)
+
+# total_sum = sum(numbers)
+
+# print(f"The sum of the numbers is: {total_sum}")
+
+
+
+
+# car_names = ["Ferrari", "Lamborghini", "Porsche", "Bugatti", "McLaren"]
+
+# first_three_cars = car_names[:3]
+# print("First three cars:", first_three_cars)
+
+# last_two_cars = car_names[-2:]
+# print("Last two cars:", last_two_cars)
+
+# difference = len(car_names[-3]) - len(car_names[-4])
+# print("Difference in string lengths between the third and fourth cars from the right:", difference)
+
+
+
+
+# name = "YourName"
+
+# name_list = [name, name]
+
+# for person in name_list:
+#     if person == name:
+#         print("Hello Admin")
+#     else:
+#         print("Hello User")
